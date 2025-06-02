f28e1ca9a63a820c0082905e2d2d7bba74656c5b
Petrea Silviu <petrea.silviu@gmail.com>
2025-06-02 15:44:41 +0300

    add login/logout

--- app/Infrastructure/Persistence/PdoUserRepository.php
+++ app/Infrastructure/Persistence/PdoUserRepository.php
@@ -39,12 +39,47 @@ class PdoUserRepository implements UserRepositoryInterface
 
     public function findByUsername(string $username): ?User
     {
-        // TODO: Implement findByUsername() method.
-        return null;
+        $query = 'SELECT * FROM users WHERE username = :username';
+        $statement = $this->pdo->prepare($query);
+        $statement->execute(['username' => $username]);
+        $data = $statement->fetch();
+        if (false === $data) {
+            return null;
+        }
+
+        return new User(
+            $data['id'],
+            $data['username'],
+            $data['password_hash'],
+            new DateTimeImmutable($data['created_at']),
+        );
+        
     }
 
     public function save(User $user): void
     {
-        // TODO: Implement save() method.
+        if ($user->getId() === null) {
+            // Insert new user
+            $query = 'INSERT INTO users (username, password_hash, created_at) VALUES (:username, :password_hash, :created_at)';
+            $statement = $this->pdo->prepare($query);
+            $statement->execute([
+                'username' => $user->getUsername(),
+                'password_hash' => $user->getPassword(),
+                'created_at' => $user->getCreatedAt()->format('Y-m-d H:i:s'),
+            ]);
+        } else {
+            // Check if another user with the same username exists
+            $query = 'SELECT id FROM users WHERE username = :username AND id != :id';
+            $statement = $this->pdo->prepare($query);
+            $statement->execute([
+                'username' => $user->getUsername(),
+                'id' => $user->getId(),
+            ]);
+            $existing = $statement->fetch();
+            if ($existing) {
+                throw new \RuntimeException('This username exists.');
+            }
+           
+        }
     }
 }
